7815336ac349a7a8c1da768845f64d24b438458e
ashokkumar-2024 <ashokkumar@sripadastudios.in>
2026-04-28 12:09:54 +0530

    Endorsements mobile: increase container height to 260px to prevent card clipping

--- components/endorsements-section.tsx
+++ components/endorsements-section.tsx
@@ -61,7 +61,7 @@ export function EndorsementsSection() {
         {/* 3D Slider Container */}
         <div
           className="relative flex items-center justify-center transition-all duration-700 ease-out"
-          style={{ height: isMobile ? '180px' : '280px' }}
+          style={{ height: isMobile ? '260px' : '280px' }}
           style={{
             transformStyle: 'preserve-3d',
             transform: isArrowHovered ? 'rotateX(5deg) rotateY(3deg)' : 'rotateX(0deg) rotateY(0deg)'
